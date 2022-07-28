50ce51663665bb91a9b598f095a7c73f14ae3af9
Lucas Campos <lucascamposmga@gmail.com>
2022-07-28 00:12:00 +0000

    Limpando

---
+++ src/main/java/br/edu/unicesumar/aula/domain/CursoNovo.java
@@ -0,0 +1,31 @@
+package br.edu.unicesumar.aula.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CursoNovo {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private Double chMin;
+
+    private Double chMax;
+
+    private LocalDate dtAprovacao;
+
+
+}
--- src/main/java/br/edu/unicesumar/aula/repository/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.unicesumar.aula.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.unicesumar.aula.domain.Curso;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-
-}
